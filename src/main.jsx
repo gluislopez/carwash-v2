9f335c137e17b2e0e13895143661d527c1c311a1
Gerardo López Miranda <gerardolopezmiranda@Gerardos-MacBook-Pro.local>
2025-12-01 21:13:50 -0400

    Fix: Add BrowserRouter to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './css/main.css' // Import global styles
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('app')).render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </React.StrictMode>,
 )
